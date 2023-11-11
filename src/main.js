6ba9edb9e600b8056b173d3942c9d0946f52a55f
Kent Stephen Sumalinog <kentstephensumalinog@gmail.com>
2023-11-11 17:01:58 +0800

    Commit Initial practice

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import "./main.css";
 
-createApp(App).mount('#app')
+import { createRouter, createWebHashHistory } from "vue-router";
+import routes from "./routes";
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+import { createApp } from "vue";
+import App from "./App.vue";
+createApp(App).use(router).mount("#app");
